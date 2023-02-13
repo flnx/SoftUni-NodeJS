0eed7d8c0b63aea82d6c898db0abd857f0ba5114
flnx <32278279+flnx@users.noreply.github.com>
2023-02-13 15:15:47 +0200

    adding json file and mapping through the html in cat shelter

--- Introduction to NodeJS/server.js
+++ Introduction to NodeJS/server.js
@@ -1,16 +1,32 @@
 const http = require('http');
 
+const cats = require('./cats.json');
+const siteCss = require('./css/site.css');
+
+const homePage = require('./views/index');
+const editPage = require('./views/edit');
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {
-        'content-type': 'text/html',
+        'Content-Type': 'text/html',
     });
 
     if (req.url == '/') {
-        res.write('<h1>Hello from NodeJS</h1>');
-    } else if (req.url == '/cats') {
-        res.write('<h1>Timmy says Hi!!!</h1>');
+        res.write(homePage);
+    } else if (req.url == '/css/site.css') {
+        res.writeHead(200, {
+            'Content-Type': 'text/css',
+        });
+        res.write(siteCss);
+    } else if (/cats\/\d+\/edit/.test(req.url)) {
+        let catId = req.url.split('/')[2];
+        let cat = cats.find(x => x.id == catId);
+
+        res.write(editPage(cat));
     } else {
-        res.write('<h1>404 Not Found!!!!</h1>');
+        res.write(`
+            <h1>Not found</h1>
+        `);
     }
 
     res.end();
