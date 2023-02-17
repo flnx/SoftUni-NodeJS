a32fe28254cb3eb6180235939f5afefb60934d94
flnx <32278279+flnx@users.noreply.github.com>
2023-02-17 02:51:36 +0200

    Adding Breed/Cat/Shelter pages

--- Introduction to NodeJS/server.js
+++ Introduction to NodeJS/server.js
@@ -5,8 +5,13 @@ const siteCss = require('./css/site.css');
 
 const homePage = require('./views/index');
 const editPage = require('./views/edit');
+const addCatPage = require('./views/addCat');
+const addBreedPage = require('./views/addBreed');
+const shelterCatPage = require('./views/catShelter');
 
 const server = http.createServer((req, res) => {
+    console.log(req.method);
+
     res.writeHead(200, {
         'Content-Type': 'text/html',
     });
@@ -18,11 +23,24 @@ const server = http.createServer((req, res) => {
             'Content-Type': 'text/css',
         });
         res.write(siteCss);
-    } else if (/cats\/\d+\/edit/.test(req.url)) {
+    } else if (isEditById(req.url)) {
         let catId = req.url.split('/')[2];
-        let cat = cats.find(x => x.id == catId);
+        let cat = cats.find((x) => x.id == catId);
 
         res.write(editPage(cat));
+
+    } else if (isDeleteById(req.url)) {
+        const catId = req.url.split('/')[2];
+        let cat = cats.find((x) => x.id == catId);
+
+        res.write(shelterCatPage(cat));
+
+    } else if (req.url == '/cats/add-breed') {
+        res.write(addBreedPage);
+
+    } else if (req.url == '/cats/add-cat') {
+        res.write(addCatPage);
+
     } else {
         res.write(`
             <h1>Not found</h1>
@@ -34,3 +52,11 @@ const server = http.createServer((req, res) => {
 
 server.listen(5000);
 console.log('Server is running on port 5000...');
+
+function isEditById(url) {
+    return /cats\/\d+\/edit/.test(url);
+}
+
+function isDeleteById(url) {
+    return /cats\/\d+\/delete/.test(url);
+}
