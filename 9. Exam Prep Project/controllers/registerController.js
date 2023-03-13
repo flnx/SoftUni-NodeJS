f2fc0bb09efb62c875ca6bfeb03b4cd51a3e1d0c
flnx <32278279+flnx@users.noreply.github.com>
2023-03-13 20:15:37 +0200

    Adding register and error handling util

--- 9. Exam Prep Project/controllers/registerController.js
+++ 9. Exam Prep Project/controllers/registerController.js
@@ -1,7 +1,22 @@
+const { registerUser } = require('../services/userService');
 const registerController = require('express').Router();
+
+const handleErrors = require('../utils/errorHandler');
 
 registerController.get('/', (req, res) => {
     res.render('register');
+});
+
+registerController.post('/', async (req, res) => {
+    try {
+        await registerUser(req.body);
+
+        res.redirect('/');
+    } catch (err) {
+        res.render('register', {
+            errors: handleErrors(err),
+        });
+    }
 });
 
 module.exports = registerController;
---
+++ 9. Exam Prep Project/services/userService.js
@@ -0,0 +1,30 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/userSchema');
+
+async function registerUser({ username, password, rePass, email }) {
+    if (password != rePass) {
+        throw Error("Passwords don't match");
+    }
+
+    if (password.length < 6) {
+        throw Error('Password must be at least 6 characters long');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+        username,
+        password: hashedPassword,
+        email,
+    });
+
+    return {
+        username,
+        email,
+        _id: user._id,
+    };
+}
+
+module.exports = {
+    registerUser,
+}
