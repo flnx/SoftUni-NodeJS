514a26e42d3ff4781cc131758ca91772ade7712f
flnx <32278279+flnx@users.noreply.github.com>
2023-03-13 23:49:14 +0200

    Update routes.js

--- 9. Exam Prep Project/config/routes.js
+++ 9. Exam Prep Project/config/routes.js
@@ -15,12 +15,12 @@ const { isAuthenticated, isGuest } = require('../middlewares/guards');
 
 module.exports = (app) => {
     app.use(homeController);
-    app.use('/catalog', catalogController);
     app.use('/login', isGuest(), loginController);
     app.use('/register', isGuest(), registerController);
-    app.use('/search', searchController);
     app.use('/create', isAuthenticated(), createController);
     app.use('/edit', isAuthenticated(), editController);
+    app.use('/catalog', catalogController);
+    app.use('/search', searchController);
     app.use('/details', detailsController);
     app.use('/logout', logoutController);
     app.use('*', notFoundController);
